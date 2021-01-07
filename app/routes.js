8a9520c0c283a910c4a05624966c00a63186c909
Sophie Ottaway <sophieottaway@yahoo.co.uk>
2021-01-07 17:15:27 +0000

    Added journeys to view trader permissions and remove an association

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+var activetraders;
 // Add your routes here - above the module.exports line
+
+//DOA ROUTES
+//REMOTE A Trader
+router.post('/delete-trader', function (req, res) {
+  if (req.session.data['remove-association']=="yes"){
+    activetraders= false;
+  }
+  else {
+    activetraders= true;
+  }
+  res.redirect('/doa/dashboard/index')
+})
+
+router.get('/doa/dashboard/index', function (req, res) {
+
+  res.render('doa/dashboard/index', { activetraders })
+})
 
 module.exports = router
