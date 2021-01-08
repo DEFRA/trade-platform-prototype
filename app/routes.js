a083c7a96a11871d384deb178ccacef2ca19b087
Sophie Ottaway <sophieottaway@yahoo.co.uk>
2021-01-08 10:04:00 +0000

    small tweak to routing for active traders view

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,11 @@ var activetraders;
 // Add your routes here - above the module.exports line
 
 //DOA ROUTES
+//Before you Start
+router.get('/doa/index', function (req, res) {
+  activetraders=true;
+  res.render('doa/index', { activetraders })
+})
 //REMOTE A Trader
 router.post('/delete-trader', function (req, res) {
   if (req.session.data['remove-association']=="yes"){
@@ -17,7 +22,6 @@ router.post('/delete-trader', function (req, res) {
 })
 
 router.get('/doa/dashboard/index', function (req, res) {
-
   res.render('doa/dashboard/index', { activetraders })
 })
 
